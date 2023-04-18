6cd5a291c9fba449f8c5e0d65a50349075b5df2f
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2023-04-18 14:11:24 -0500

    C# Lesson 1 & 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,80 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage: ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+        string letter = "";
+
+
+        if (grade >= 90 )
+            {
+                if (grade >= 93)
+                {
+                    letter = "A";
+                }
+
+                else {
+                    letter = "A-";
+                }
+            }
+            else if (grade >= 80)
+            {
+                 if (grade >= 87)
+                    letter = "B+";
+                // Console.Write("You earned an A, you have passed the class");
+                else if (grade >= 83)
+                {
+                    letter = "B";
+                }
+
+                else {
+                    letter = "B-";
+                }
+                // Console.Write("You earned a B, you have passed the class ");
+               
+            }
+            else if (grade  >= 70)
+            {
+                 if (grade >= 77)
+                    letter = "C+";
+                // Console.Write("You earned an A, you have passed the class");
+                else if (grade >= 73)
+                {
+                    letter = "C";
+                }
+
+                else {
+                    letter = "C-";
+                }
+                // Console.Write("You earned a C, you have passed the class");
+               
+            }
+            else if (grade >= 60)
+            {
+                // Console.Write("You earned a D, you have failed the class. You need at least a 70 to pass Good luck next time");
+                letter = "D";
+            }
+            else 
+            {
+                // Console.Write("You earned an F, You have failed the course. Good luck next time");
+                letter = "F";
+                
+            }     
+            Console.WriteLine($"Your grade is: {letter}");
+            
+            if (grade >= 70)
+            {
+                Console.WriteLine("You Passed");
+            }
+            else
+            {
+                Console.WriteLine("Better Luck Next Time");
+            }
+
+
+            
     }
 }
