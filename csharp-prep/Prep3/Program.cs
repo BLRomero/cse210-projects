1e880a83883ecaea49a97cd86255c0f6a4edbfeb
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2023-04-25 11:15:51 -0500

    Prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+    
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            
+            Console.Write("Pick a number between 1-100. What is your guess? ");
+            guess =  int.Parse(Console.ReadLine());
+            
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("higher");
+            }
+
+            else 
+            {
+                Console.WriteLine("You Guessed It!!");
+            }
+        }; 
     }
 }
+
